e901e75561e978d9540694b215a591bf4218e34c
Farhad Mammadov <github@farhad.su>
2023-08-17 07:02:33 +0300

    The second task is done

---
+++ ConsoleApp/TaskOne.cs
@@ -0,0 +1,93 @@
+﻿using AdvancedSharpAdbClient;
+
+namespace ConsoleApp;
+
+public static class TaskOne
+{
+    public static async Task TaskOneAsync()
+    {
+        AdbClient client = new();
+        // Connect to the device
+        client.Connect("127.0.0.1:62001");
+
+        // Get the connected device
+        DeviceData? device = client.GetDevices().FirstOrDefault();
+
+        // Print the device status
+        Console.WriteLine("Device status: " + device?.State);
+
+        // Get all running tasks
+        var runningTasks = await TaskOne.GetAllRunningTasksAsync(client, device);
+
+        // Kill all the running tasks
+        if (runningTasks.Count > 0)
+        {
+            foreach (var task in runningTasks)
+            {
+                await TaskOne.KillRunningTaskAsync(client, device, task);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No running tasks found");
+            return;
+        }
+
+        Console.WriteLine("**** DONE ****");
+    }
+
+    // Get all running tasks
+    public static async Task<List<string>> GetAllRunningTasksAsync(AdbClient client, DeviceData? device)
+    {
+        List<string> runningTasks = new();
+        Console.WriteLine("Getting all running tasks...");
+
+        try
+        {
+            ConsoleOutputReceiver receiver = new();
+            await client.ExecuteRemoteCommandAsync("dumpsys activity activities", device, receiver);
+            string output = receiver.ToString();
+
+            string[] lines = output.Split('\n')
+                                 .Where(x => x.Contains("(fullscreen)"))
+                                 .Where(x => x.Contains("com.").Equals(true))
+                                 .ToArray();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int index = lines[i].IndexOf(':');
+                lines[i] = lines[i][(index + 1)..];
+                index = lines[i].IndexOf('}');
+                lines[i] = lines[i][..index];
+            }
+
+            runningTasks = lines.ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("Completed getting all running tasks");
+        return runningTasks;
+    }
+
+    // Kill one running task
+    public static async Task KillRunningTaskAsync(AdbClient client, DeviceData? device, string appName)
+    {
+        Console.WriteLine($"Killing {appName}...");
+
+        try
+        {
+            await client.ExecuteRemoteCommandAsync($"am force-stop {appName}", device, null);
+            await client.ExecuteRemoteCommandAsync($"pm clear {appName}", device, null);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine($"Completed killing {appName}");
+        await Task.CompletedTask;
+    }
+}
---
+++ ConsoleApp/TaskTwo.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace ConsoleApp;
+
+public class TaskTwo
+{
+    public static async Task TaskTwoAsync()
+    {
+        var appiumOptions = new AppiumOptions();
+        appiumOptions.AddAdditionalCapability("platformName", "Android");
+        appiumOptions.AddAdditionalCapability("deviceName", "emulator-5554");
+        appiumOptions.AddAdditionalCapability("platformVersion", "13.0");
+        appiumOptions.AddAdditionalCapability("automationName", "UiAutomator2");
+        appiumOptions.AddAdditionalCapability("noReset", "true");
+
+        var _driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions, TimeSpan.FromSeconds(180));
+        _driver.FindElementByXPath("//android.widget.TextView[@content-desc=\'Chrome\']").Click();
+
+        await Task.Delay(TimeSpan.FromSeconds(3));
+
+        try
+        {
+            _driver.FindElementById("com.android.chrome:id/terms_accept").Click();
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            _driver.FindElementById("com.android.chrome:id/negative_button").Click();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            _driver.FindElementById("com.android.chrome:id/negative_button").Click();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("No terms and conditions");
+        }
+
+        try
+        {
+            _driver.FindElementById("com.android.chrome:id/search_box_text").SendKeys("my ip address");
+            _driver.PressKeyCode(AndroidKeyCode.Enter);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Something is went wrong: " + ex.Message);
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(3));
+
+        try
+        {
+            _driver.Context = _driver.Contexts.Last();
+            var spanElements = _driver.FindElementsByTagName("span").Where(x => x.Text != string.Empty).Select(x => x.Text).ToArray();
+
+            for (int i = 0; i < spanElements.Length; i++)
+            {
+                if (spanElements[i] == "Your public IP address")
+                {
+                    Console.WriteLine(spanElements[i]);
+                    Console.WriteLine(spanElements[i - 1]);
+                    break;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("No IP address found");
+        }
+        finally
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
